16b004dcd1dff3717def08ba6a1002362a13209d
piyatida646kp <59428776+piyatida646@users.noreply.github.com>
2021-11-05 17:40:17 +0700

    Create Person.java

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+
+
+public class Person {
+
+	private Vehicle vehicle;
+	
+	public Person(Vehicle v) {
+		vehicle = v;
+	}
+	
+	public void setDrive(Vehicle v) {
+		vehicle = v;
+	}
+	
+	public void drive() {
+		vehicle.drive();
+	}
+
+}
